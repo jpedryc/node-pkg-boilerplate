e636a0b72267cf17c4f577f3851b4950c5a12b2a
Jan Pedryc <jan.pedryc@gmail.com>
2018-05-20 16:50:28 +0200

    Documentation added

--- src/MyClass/MyClass.js
+++ src/MyClass/MyClass.js
@@ -1,4 +1,11 @@
 export class MyClass {
+    /**
+     * This is just a sample function that returns the percentage value of a number
+     * 
+     * @param percent
+     * @param amount
+     * @returns {number}
+     */
     callMe({ percent = 100, amount }) {
         return (percent / 100) * amount
     }
